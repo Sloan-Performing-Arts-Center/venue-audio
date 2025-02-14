778e7ac428b1a886fffda874816861429ee30286
hingobway <hingobway@users.noreply.github.com>
2025-02-14 11:37:59 -0500

    new GH pages website

---
+++ .site/src/content.config.ts
@@ -0,0 +1,28 @@
+import { glob } from 'astro/loaders';
+import { defineCollection, z } from 'astro:content';
+import { ASTRO_FOLDER_NAME } from '../../CONFIGURATION';
+
+const pages = defineCollection({
+  loader: glob({
+    base: '..',
+    pattern: `{*.md,!(${ASTRO_FOLDER_NAME})/**/*.md}`,
+    generateId(entry) {
+      return pathToSlug(entry.entry);
+    },
+  }),
+
+  schema: z.object({
+    title: z.string().optional(),
+    description: z.string().optional(),
+  }),
+});
+
+export const collections = { pages };
+
+function pathToSlug(path: string) {
+  let slug = path
+    .replace(/((?:\/|^)readme)?\.md$/i, '$1')
+    .replace(/readme$/i, 'index');
+  if (!slug.length) slug = '/';
+  return slug;
+}
---
+++ CONFIGURATION.js
@@ -0,0 +1,5 @@
+/** name of interior folder that contains docs site code */
+export const ASTRO_FOLDER_NAME = '.site';
+
+/** header title bar title */
+export const MAIN_TITLE = 'SPAC Audio Systems Documentation';
